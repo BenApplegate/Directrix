eeef17d7505200ef2427aa23271174c53653c214
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2024-08-28 16:33:07 -0600

    create Mesh class

---
+++ Directrix/Objects/Mesh.cs
@@ -0,0 +1,25 @@
+using System.Numerics;
+using System.Collections;
+
+namespace Directrix.Objects;
+
+public class Mesh
+{
+	private List<Vector3> m_verts;
+	
+	public List<Vector3> Verticies
+	{
+		get { return m_verts; }
+		set { m_verts = new List<Vector3>(value); }
+	}
+	
+	public Mesh()
+	{
+		m_verts = new();
+	}
+	
+	public Mesh(List<Vector3> verts)
+	{
+		m_verts = new(verts);
+	}
+}
